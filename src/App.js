805459713231a6fc58ea15d0e7b0d251966bc1fd
Sakha Gangadhar <vsakha@uncc.edu>
2023-10-13 22:11:44 -0400

    cleared merge conflict

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import './App.css';
 import Menu from './Menu/Menu';
 import Hero from './Hero/Hero';
@@ -30,36 +29,6 @@ function App() {
             <HomePage/>
           </Route>
         </Switch>
-=======
-import './App.scss';
-
-import 
-  {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-  } from "react-router-dom";
-
-import Aboutpage from './AboutPage/AboutPage';
-import Loginpage from './LoginPage/LoginPage';
-import Menu from './Menu/Menu';
-import HomePage from './HomePage/HomePage';
-import Footer from './Footer/Footer';
-import Hero from './Hero/Hero';
-
-function App() {
-  return (
-    <Router className="App">
-      <Menu/>
-      <Hero/>
-      <div className="main Container">
-        
-        <Routes>
-          <Route path="/About" element={<Aboutpage/>}/>
-          <Route path="/Login" element={<Loginpage/>}/>
-          <Route path="/" element={<HomePage/>} />
-        </Routes>
->>>>>>> 004779efc7208f2c4230ca173b887821d856de6d
       </div>
       <Footer/>
     </Router>
