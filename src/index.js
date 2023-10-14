4934f1c4e8af6ef00249621da9a59bf9c8a3baf1
Sakha Gangadhar <vsakha@uncc.edu>
2023-10-13 22:08:53 -0400

    cleared merge conflict

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import './index.css';
-=======
-import './index.scss';
->>>>>>> 004779efc7208f2c4230ca173b887821d856de6d
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
